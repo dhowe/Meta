df369fa4f1fce151515610fd8be1b2505dd5f999
Kenny Wong <kennyviperhk@gmail.com>
2020-01-29 18:58:35 +0800

    udpate website crawler

--- WebCrawler/pycopy.py
+++ WebCrawler/pycopy.py
@@ -1,9 +1,9 @@
 from pywebcopy import save_website
 
-kwargs = {'project_name': 'some-fancy-name'}
+kwargs = {'project_name': 'wdm'}
 
 save_website(
-    url='https://rednoise.org/wdm/',
+    url='https://rednoise.org/teaching/wdm/',
     project_folder='./downloads',
     **kwargs
 )
