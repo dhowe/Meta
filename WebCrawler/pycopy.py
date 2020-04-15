1f8c40efdcdbb8c8b986432ab6ccf83cc53bfb8a
dhowe <dhowe@cims.nyu.edu>
2020-04-15 15:28:26 +0800

    cleanup

--- WebCrawler/pycopy.py
+++
@@ -1,9 +0,0 @@
-from pywebcopy import save_website
-
-kwargs = {'project_name': 'wdm'}
-
-save_website(
-    url='https://rednoise.org/teaching/wdm/',
-    project_folder='./downloads',
-    **kwargs
-)
